e458592bc732f3da046017095a09bc9cfd4fdc44
Eric Tobin <eatobin@gmail.com>
2019-12-01 17:34:35 -0600

    building

--- src/ch2/Ch2.hs
+++ src/ch2/Ch2.hs
@@ -2,3 +2,14 @@ module Ch2 where
 
 sayHello :: String -> IO ()
 sayHello x = putStrLn ("Hello, " ++ x ++ "!!")
+
+addEm :: Int -> Int
+addEm x =
+  (+) x x
+
+subEm :: Int -> Int
+subEm x = (-) x x
+
+subOne :: Int -> Int
+subOne x = (-) x 1
+-- a comment
